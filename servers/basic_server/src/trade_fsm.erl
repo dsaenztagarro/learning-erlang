fd6ebe672a2f9a077ab50d5e5c01d76c43ca2229
David Saenz <david.saenz.tagarro@gmail.com>
2016-04-18 07:13:46 +0200

    Implemented callbacks for gen_fsm

--- servers/basic_server/src/trade_fsm.erl
+++ servers/basic_server/src/trade_fsm.erl
@@ -1,8 +1,15 @@
 -module(trade_fsm).
 -behaviour(gen_fsm).
 
+-record(state, {name="",
+                other,
+                ownitems=[],
+                otheritems=[],
+                monitor,
+                from}).
+
 %% public API
--export([start/0, event/0]).
+-export([start/1, start_link/1]).
 
 %% gen_fms callbacks
 -export([init/1, handle_event/3, handle_sync_event/4, handle_info/3,
@@ -17,3 +24,45 @@ start_link(Name) ->
 % Cancel the transaction
 cancel(OwnPid) ->
     gen_fsm:sync_send_all_state_event(OwnPid, cancel).
+
+%% gen_fsm behaviour
+
+init(Name) ->
+    {ok, idle, #state{name=Name}}.
+
+handle_event(cancel, _StateName, S=#state{}) ->
+    notice(S, "received cancel event", []),
+    {stop, other_cancelled, S};
+
+handle_event(Event, StateName, Data) ->
+    unexpected(Event, StateName),
+    {next_state, StateName, Data}.
+
+handle_sync_event(Event, _From, StateName, Data) ->
+    unexpected(Event, StateName),
+    {next_state, StateName, Data}.
+
+handle_info(Info, StateName, Data) ->
+    unexpected(Info, StateName),
+    {next_state, StateName, Data}.
+
+code_change(_OldVsn, StateName, Data, _Extra) ->
+    {ok, StateName, Data}.
+
+terminate(normal, ready, S=#state{}) ->
+    notice(S, "FSM leaving.", []);
+
+terminate(_Reason, _StateName, _StateData) ->
+    ok.
+
+% helpers
+
+% Send players a notice.
+notice(#state{name=N}, Str, Args) ->
+    io:format("~s: "++Str++"~n", [N|Args]).
+
+%% Allows to log unexpected messages.
+unexpected(Msg, State) ->
+    io:format("~p received unknown eent ~p while in state ~p~n",
+              [self(), Msg, State]).
+
